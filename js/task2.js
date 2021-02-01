b844fbdbb20a6905fa565f5eb90e1751282c88cc
Behuamuh <sven1980@list.ru>
2021-02-01 05:13:19 +0300

    done tasks

--- js/task2.js
+++ js/task2.js
@@ -1,1 +1,40 @@
+/* eslint-disable no-undef */
 // Пример 2 - инфинити скрол с троттлингом
+let handlerCallCount = 0;
+let startIndex = 0;
+const COUNTRY_COUNT = 10;
+
+const listRef = document.querySelector('.country-list');
+
+function isScrollToBottom() {
+  return window.innerHeight + window.scrollY >= document.body.offsetHeight;
+}
+
+function createCountryRef(country) {
+  const countryRef = document.createElement('h2');
+  countryRef.classList.add('country');
+  countryRef.textContent = country;
+
+  return countryRef;
+}
+
+function renderPartOfCounries() {
+  const partyOfCountries = countries
+    .slice(startIndex, startIndex + COUNTRY_COUNT);
+  const countryRefs = partyOfCountries.map(createCountryRef);
+
+  listRef.append(...countryRefs);
+}
+
+function scrollHandler() {
+  console.log(++handlerCallCount);
+  if (!isScrollToBottom()) return;
+
+  renderPartOfCounries();
+
+  startIndex += COUNTRY_COUNT;
+}
+
+renderPartOfCounries();
+
+window.addEventListener('scroll', _.throttle(scrollHandler, 200));
--- js/task3.js
+++ js/task3.js
@@ -1,1 +1,36 @@
+/* eslint-disable no-undef */
 // Пример 3 - живой поиск простой
+let handlerCallCount = 0;
+
+const listRef = document.querySelector('.country-list');
+const searchRef = document.querySelector('.search');
+
+function createCountryRef(country) {
+  const countryRef = document.createElement('h2');
+  countryRef.classList.add('country');
+  countryRef.textContent = country;
+
+  return countryRef;
+}
+
+function renderFilteredCounries(filteredCounries) {
+  const countryRefs = filteredCounries.map(createCountryRef);
+
+  listRef.append(...countryRefs);
+}
+
+renderFilteredCounries(countries);
+
+searchRef.addEventListener('input', event => {
+  console.log(++handlerCallCount);
+  const search = event.target.value;
+  const filteredCountries = countries.filter(country => {
+    if (search === '') return true;
+
+    return country.toLowerCase()
+      .includes(search.toLowerCase());
+  });
+  
+  listRef.innerHTML = '';
+  renderFilteredCounries(filteredCountries);
+});
